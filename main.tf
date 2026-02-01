c8c2b8f1d156a34646ab196547d70f7073cf2b95
Luis Becerra Solis <lubeso@icloud.com>
2026-02-01 17:53:29 -0500

    fix: use existing url map

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ resource "google_compute_target_https_proxy" "main" {
 
 resource "google_compute_target_http_proxy" "main" {
   name    = google_compute_global_address.main.name
-  url_map = google_compute_url_map.http.self_link
+  url_map = google_compute_url_map.main.self_link
 }
 
 resource "google_compute_global_forwarding_rule" "https" {
