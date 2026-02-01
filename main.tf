f83a2c58003492c2e3daf727b654e3d48ed43b41
Luis Becerra Solis <lubeso@icloud.com>
2026-02-01 16:08:08 -0500

    feature: add url map

--- main.tf
+++ main.tf
@@ -6,6 +6,57 @@ resource "google_compute_global_address" "main" {
   name = "default"
 }
 
+resource "google_compute_url_map" "main" {
+  name            = google_compute_global_address.main.name
+  default_service = google_compute_backend_bucket.static.id
+  host_rule {
+    hosts        = [var.domain, "www.${var.domain}"]
+    path_matcher = "default"
+  }
+  dynamic "host_rule" {
+    for_each = var.websites
+    content {
+      hosts        = ["${host_rule.key}.${var.domain}"]
+      path_matcher = host_rule.key
+    }
+  }
+  path_matcher {
+    name            = "default"
+    default_service = google_compute_backend_bucket.static.id
+    path_rule {
+      paths = ["/*"]
+      route_action {
+        url_rewrite {
+          path_prefix_rewrite = "/www/"
+        }
+      }
+      service = google_compute_backend_bucket.static.id
+    }
+  }
+  dynamic "path_matcher" {
+    for_each = var.websites
+    content {
+      name            = path_matcher.key
+      default_service = google_compute_backend_bucket.static.id
+      path_rule {
+        paths = ["/*"]
+        route_action {
+          url_rewrite {
+            path_prefix_rewrite = "/${path_matcher.key}/"
+          }
+        }
+        service = google_compute_backend_bucket.static.id
+      }
+    }
+  }
+}
+
+resource "google_compute_backend_bucket" "static" {
+  name        = module.storage_bucket_static.name
+  bucket_name = module.storage_bucket_static.name
+  enable_cdn  = true
+}
+
 module "storage_bucket_static" {
   source        = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
   version       = "~> 6.1.0"
@@ -24,10 +75,4 @@ module "storage_bucket_static" {
       role   = "roles/storage.objectViewer"
     }
   ]
-}
-
-resource "google_compute_backend_bucket" "static" {
-  name        = module.storage_bucket_static.name
-  bucket_name = module.storage_bucket_static.name
-  enable_cdn  = true
 }
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "domain" {
+  type = string
+}
+
+variable "websites" {
+  type = list(string)
+}
