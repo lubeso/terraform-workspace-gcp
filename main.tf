74cb4a3704e8f2af2a31197459f3905ce74907a8
Luis Becerra Solis <lubeso@icloud.com>
2026-02-01 18:04:51 -0500

    fix: update dangling resource reference

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "google_compute_url_map" "http" {
 resource "google_compute_target_https_proxy" "main" {
   name             = google_compute_global_address.main.name
   url_map          = google_compute_url_map.https.id
-  ssl_certificates = [google_compute_managed_ssl_certificate.default.id]
+  ssl_certificates = [google_compute_managed_ssl_certificate.main.id]
 }
 
 resource "google_compute_target_http_proxy" "main" {
