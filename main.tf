517cf3bf0fbf1e25fda3d40a6bb2ebab2cd37944
Luis Becerra Solis <lubeso@icloud.com>
2026-03-01 10:10:58 -0500

    chore: update target https proxy certificates

--- main.tf
+++ main.tf
@@ -69,10 +69,7 @@ resource "google_compute_url_map" "http" {
 resource "google_compute_target_https_proxy" "main" {
   name             = google_compute_global_address.main.name
   url_map          = google_compute_url_map.https.id
-  ssl_certificates = [
-    google_compute_managed_ssl_certificate.main.id,
-    google_compute_managed_ssl_certificate.staging.id,
-  ]
+  ssl_certificates = [google_compute_managed_ssl_certificate.staging.id]
 }
 
 resource "google_compute_target_http_proxy" "main" {
